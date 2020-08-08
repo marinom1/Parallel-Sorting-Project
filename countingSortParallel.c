cf1e50e27b3a90006c9d10ed242fae89eee5cf0d
marinom1 <marinom1@reinhardt>
2020-08-07 21:58:01 -0400

    counting sort complete, quicksort serial started

--- countingSortParallel.c
+++ countingSortParallel.c
@@ -27,6 +27,7 @@ int main(int argc, char** argv){
     if (rank ==0) {
         //n is the number of elements in the array
         n = 1000000000;  //1billion
+        //n = 100000000; //100million
         //n = 50;
     }
     int max = 100; //the highest value you could possibly have to sort
@@ -72,17 +73,17 @@ int main(int argc, char** argv){
     }
 
     if (printOutStuff == 1) {
-    if (rank == 0) { //prints out the calculations for how much work each rank will do
-        for (int i = 0; i < numranks; i = i + 1) {
-            printf("sendcounts[%d] = %d    -   displs[%d] = %d\n", i, sendcounts[i], i, displs[i]);
+        if (rank == 0) { //prints out the calculations for how much work each rank will do
+            for (int i = 0; i < numranks; i = i + 1) {
+                printf("sendcounts[%d] = %d    -   displs[%d] = %d\n", i, sendcounts[i], i, displs[i]);
+            }
         }
-    }
     }
     MPI_Bcast(sendcounts, numranks, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(displs, numranks, MPI_INT, 0, MPI_COMM_WORLD);
     int myN =sendcounts[rank];
 
-    for (int i = 1; i < n; i = i + 1) {
+    for (int i = 1; i < n; i = i + 1) { //Find the biggest value, so we know when to stop counting
         if (scrambledArray[i] > biggestValue) {
              biggestValue = scrambledArray[i];
         }
@@ -106,36 +107,33 @@ int main(int argc, char** argv){
 
     //Prints out the myScrambled array
     if (printOutStuff == 1) {
-    printf("Rank %d: myScrambledArray contains: ", rank);
-    for (int i = 0; i  < myN; i = i + 1) {
-        printf("%d ", myScrambledArray[i]);
-        if (i % 30 == 29 ) {
-            printf("\n");
+        printf("Rank %d: myScrambledArray contains: ", rank);
+        for (int i = 0; i  < myN; i = i + 1) {
+            printf("%d ", myScrambledArray[i]);
+            if (i % 30 == 29 ) {
+                printf("\n");
+            }
         }
-    }
     }
     printf("\n"); 
 
+    //Parallel part here
     int *countArray = malloc((max+1)*sizeof(int));
     int *myCountArray = countingSort(myScrambledArray, myN, biggestValue, max, countArray);
 
     if (printOutStuff == 1) {
-    printf("Rank %d: myCountArray is: ", rank);
+        printf("Rank %d: myCountArray is: ", rank);
         for (int i = 0; i < max+1; i = i + 1) {
-            printf("%d ",myCountArray[i]);
+                printf("%d ",myCountArray[i]);
         }
-    printf("\n");
+        printf("\n");
     }
-    //MPI_Allreduce(MPI_IN_PLACE, myCountArray,n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
     for (int i = 0; i < max+1; i = i + 1) { //this adds all the counts from all the ranks
         int x = myCountArray[i];
         MPI_Reduce(&x, &myCountArray[i],1, MPI_INT, MPI_SUM, 0,MPI_COMM_WORLD);
     }
-
-    //MPI_Allreduce(MPI_IN_PLACE, myCountArray,n, MPI_INT, MPI_SUM, MPI_COMM_WORLD); //couldnt get this working properly, fix is above
-
-    
+   //end of parallel part
     //at this point, myCountArray contains all the counts from all ranks summed up
     //only want rank 0 doing the rest of the program
     
@@ -148,36 +146,36 @@ int main(int argc, char** argv){
     
         printf("\n"); 
         }
-    //Then, store the cumulative count of each array
-    
-    for (int i = 1; i <= max; i = i + 1) {
-        myCountArray[i] += myCountArray[i - 1];
-    }
-    if (printOutStuff == 1) {
-    printf("the cumulative countArray is: ");
-        for (int i = 0; i < max+1; i = i + 1) {
-            printf("%d ",myCountArray[i]);
+        //Then, store the cumulative count of each array
+        
+        for (int i = 1; i <= max; i = i + 1) {
+            myCountArray[i] += myCountArray[i - 1];
         }
-    printf("\n"); 
-    }
-    //Then, find index of each element from original array into count array, then puts the elements in sortedArray
-    int *sortedArray = malloc(n*sizeof(int));
-    for (int i = n - 1; i >= 0; i = i - 1) {
-        sortedArray[myCountArray[scrambledArray[i]] - 1] = scrambledArray[i];
-        myCountArray[scrambledArray[i]] = myCountArray[scrambledArray[i]] - 1;
-    }
-    
-    //Prints out the sorted array
-    if (printOutStuff == 1) {
-    printf("The sorted array is: \n");
-    for (int i = 0; i  < n; i = i + 1) {
-        printf("%d ", sortedArray[i]);
-        if (i % 30 == 29 ) {
-            printf("\n");
+        if (printOutStuff == 1) {
+        printf("the cumulative countArray is: ");
+            for (int i = 0; i < max+1; i = i + 1) {
+                printf("%d ",myCountArray[i]);
+            }
+        printf("\n"); 
         }
-    }
-    printf("\n");  
-    }
+        //Then, find index of each element from original array into count array, then puts the elements in sortedArray
+        int *sortedArray = malloc(n*sizeof(int));
+        for (int i = n - 1; i >= 0; i = i - 1) {
+            sortedArray[myCountArray[scrambledArray[i]] - 1] = scrambledArray[i];
+            myCountArray[scrambledArray[i]] = myCountArray[scrambledArray[i]] - 1;
+        }
+        //sortedArray is sorted at this point
+        //Prints out the sorted array
+        if (printOutStuff == 1) {
+        printf("The sorted array is: \n");
+        for (int i = 0; i  < n; i = i + 1) {
+            printf("%d ", sortedArray[i]);
+            if (i % 30 == 29 ) {
+                printf("\n");
+            }
+        }
+        printf("\n");  
+        }
     } //end if rank ==0
 
     if(rank ==0) {
--- quicksortSerial.c
+++ quicksortSerial.c
@@ -1,1 +1,85 @@
 //quicksort Serial 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+void swap(int* a, int* b);
+int partition (int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+int *generateRandomNumbers(int *scrambledArray, int n, int max, int min);
+
+int main(int argc, char** argv){
+
+    //int n = 1000000000; //1billion
+    int n = 100;
+    int max = 100;
+    int min = 0;
+    srand(5);
+
+    int *scrambledArray = malloc(n*sizeof(int));
+    generateRandomNumbers(scrambledArray, n, max, min);
+
+    //print the scrambled array
+    printf("The scrambled array is:\n");
+    for (int i = 0; i < n; i = i + 1) {
+        printf("%d ", scrambledArray[i]);
+    }
+    printf("\n");
+
+    quickSort(scrambledArray, 0, n-1);
+
+    //print the sorted array despite it still being named scrambledArray
+    printf("The sorted array is:\n");
+    for (int i = 0; i < n; i = i + 1) {
+        printf("%d ", scrambledArray[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void swap(int* a, int* b) 
+{ 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
+}
+
+int partition (int arr[], int low, int high) 
+{ 
+    int pivot = arr[high]; 
+    int i = (low - 1);  // index of the smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // if current element is smaller than the pivot 
+        if (arr[j] < pivot) 
+        { 
+            i = i + 1; 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+}
+
+void quickSort(int arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        //pi is partitioning index, arr[p] is now at right place 
+        int pi = partition(arr, low, high); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+}
+
+int* generateRandomNumbers(int *scrambledArray, int n, int max, int min) {
+    for (int i = 0; i < n; i = i + 1) {
+        scrambledArray[i] = rand() % (max + 1) + min;
+    }
+}
