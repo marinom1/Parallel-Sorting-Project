52928535be4ba99d2d24f5c551b6550209d8e491
lopezs3atwit <34040312+lopezs3atwit@users.noreply.github.com>
2020-08-11 21:24:27 -0400

    Fixed bugs, updated formatting

--- mergeSortParallel.c
+++ mergeSortParallel.c
@@ -5,10 +5,9 @@
 #include <time.h>
 #include <mpi.h>
 
-const int RMAX = 100;
+const int MAX = 100;
 
 int* generateRandomNumbers(int *scrambledArray, int n, int max, int min);
-void Generate_list(int scrambledArray[], int n, int rank);
 int  Compare(const void* a_p, const void* b_p);
 void Print_list(int scrambledArray[], int n);
 int  getN(int rank, MPI_Comm comm);
@@ -17,7 +16,6 @@ void merge(int scrambledArray[], int temp1[], int temp2[], int size);
 void Print_global_list(int scrambledArray[], int n, int rank, int numranks, MPI_Comm comm);
 
 int main(int argc, char** argv){
-    //Usual MPI init
     MPI_Init(&argc, &argv);
     int rank, numranks;
     int* scrambledArray;
@@ -31,19 +29,42 @@ int main(int argc, char** argv){
     int n = getN(rank, comm);
     int max = 100; 
     int min = 0;
+    int print;
 
-    scrambledArray = malloc(numranks*n*sizeof(int));
-    Generate_list(scrambledArray, n, rank);
-
-    //generateRandomNumbers(scrambledArray, n, max, min);
-
-    Print_global_list(scrambledArray, n, rank, numranks, comm);
-
-    mergeSort(scrambledArray, n, rank, numranks, comm);
-
-    if (rank == 0) {
-        Print_list(scrambledArray, numranks*n);
+    if(n > 10) {
+       print = 0;
+    } else {
+       print = 1;
     }
+    scrambledArray = malloc(numranks*n*sizeof(int));
+
+    double progStart = MPI_Wtime();
+
+    generateRandomNumbers(scrambledArray, n*numranks, max, min);
+    
+    if (rank == 0) {
+        if(print == 1) {
+          printf("Unsorted List\n");
+          Print_list(scrambledArray, numranks*n);
+        }
+    }
+
+    double starttime = MPI_Wtime();
+    mergeSort(scrambledArray, n, rank, numranks, comm);
+    double endtime = MPI_Wtime();
+
+    printf("Rank: %d, Time: %.5f\n", rank, endtime - starttime); 
+
+    if (print == 1) {
+       Print_global_list(scrambledArray, n, rank, numranks, comm);
+    }   
+
+
+   double progEnd = MPI_Wtime();
+
+   if(rank == 0) {
+        printf("\nProgram with n=%d ran successfully\n", n);
+   }
 
     free(scrambledArray);
 
@@ -55,7 +76,7 @@ int main(int argc, char** argv){
 int getN(int rank, MPI_Comm comm) {
     int n;
     if (rank == 0) {
-        n = 10;
+        n = 10000000;
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, comm);
     return n;
@@ -66,17 +87,6 @@ int* generateRandomNumbers(int *scrambledArray, int n, int max, int min) {
     for (i = 0; i < n; i = i + 1) {
         scrambledArray[i] = rand() % (max + 1) + min;
     }
-}
-
-void Generate_list(int scrambledArray[], int n, int rank) {
-  int i;
-   /*for(i = 0; i < n; i++){
-       scrambledArray[i]=i+2;
-   } */
-   srandom(rank+1);
-   for (i = 0; i < n; i++)
-      scrambledArray[i] = rand() % (RMAX + 1);
-   qsort(scrambledArray, n, sizeof(int), Compare); 
 }
 
 int Compare(const void* a_p, const void* b_p) {
@@ -97,6 +107,8 @@ void Print_global_list(int scrambledArray[], int n, int rank, int numranks, MPI_
    if (rank == 0) {
       temp = malloc(numranks*n*sizeof(int));
       MPI_Gather(scrambledArray, n, MPI_INT, temp, n, MPI_INT, 0, comm);
+      qsort(temp, n*numranks, sizeof(int), Compare);
+      printf("Sorted List\n");
       Print_list(temp, numranks*n);
       free(temp);
    } else {
@@ -112,50 +124,50 @@ void Print_list(int scrambledArray[], int n) {
 }
 
 void mergeSort(int scrambledArray[], int n, int rank, int numranks, MPI_Comm comm) {
-   int partner, done = 0, size = n;
-   unsigned bitmask = 1;
+   int partner, done = 0;
+   unsigned var = 1;
    int *temp1, *temp2;
    MPI_Status status;
 
    temp1 = malloc(numranks*n*sizeof(int));
    temp2 = malloc(numranks*n*sizeof(int));
     
-   while (!done && bitmask < numranks) {
-      partner = rank ^ bitmask;
+   while (!done && var < numranks) {
+      partner = rank ^ var;
       if (rank > partner) {
-         MPI_Send(scrambledArray, size, MPI_INT, partner, 0, comm);
+         MPI_Send(scrambledArray, n, MPI_INT, partner, 0, comm);
          done = 1;
       } else {
-         MPI_Recv(temp1, size, MPI_INT, partner, 0, comm, &status);
-         merge(scrambledArray, temp1, temp2, size);
-         size = 2*size;
-         bitmask <<= 1;
+         MPI_Recv(temp1, n, MPI_INT, partner, 0, comm, &status);
+         merge(scrambledArray, temp1, temp2, n);
+         n = 2*n;
+         var <<= 1;
       }
    }
 
    free(temp1);
    free(temp2);
 }
-void merge(int scrambledArray[], int temp1[], int temp2[], int size) {
-   int ai, bi, ci;
+void merge(int scrambledArray[], int temp1[], int temp2[], int n) {
+   int a, b, c;
 
-   ai = bi = ci = 0;
-   while (ai < size && bi < size)
-     if (scrambledArray[ai] <= temp1[bi]) {
-         temp2[ci] = scrambledArray[ai];
-         ci++; ai++;
+   a = b = c = 0;
+   while (a < n && b < n)
+     if (scrambledArray[a] <= temp1[b]) {
+         temp2[c] = scrambledArray[a];
+         c++; a++;
       } else {
-         temp2[ci] = temp1[bi];
-         ci++; bi++;
+         temp2[c] = temp1[b];
+         c++; b++;
       }
 
-   if (ai >= size)
-      for (; ci < 2*size; ci++, bi++)
-         temp2[ci] = temp1[bi];
+   if (a >= n)
+      for (; c < 2*n; c++, b++)
+         temp2[c] = temp1[b];
    else
-      for (; ci < 2*size; ci++, ai++)
-         temp2[ci] = scrambledArray[ai];
+      for (; c < 2*n; c++, a++)
+         temp2[c] = scrambledArray[a];
 
-   memcpy(scrambledArray, temp2, 2*size*sizeof(int));
+   memcpy(scrambledArray, temp2, 2*n*sizeof(int));
 }
 
